433f24638a4ff1c70310ef19597ad3be58b4f848
zhangyudong <zhangyudong@pateo.com.cn>
2018-10-10 17:15:10 +0800

    添加了三个测试 与封装

--- app/src/main/java/com/example/rxrtlearn/MainActivity.java
+++ app/src/main/java/com/example/rxrtlearn/MainActivity.java
@@ -27,6 +27,9 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
+/**
+ * https://blog.csdn.net/DeMonliuhui/article/details/77868677  依据文章
+ */
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
